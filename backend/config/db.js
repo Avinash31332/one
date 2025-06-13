ff939bfdc104024d0b6980faeb1e41c854f805a0
Avinash <avinashviolin4@gmail.com>
2025-06-13 22:11:27 +0530

    conn change to db

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.ATLAS);
     console.log(`MongoDB Connected`);
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
