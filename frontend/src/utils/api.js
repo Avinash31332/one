72402c5620ee1a2d4643b3971679786535c68a71
Avinash <avinashviolin4@gmail.com>
2025-06-13 22:56:38 +0530

    changed api.js baseurl

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://bus-dwmy.onrender.com", // Adjust the URL as needed
-  baseURL: "http://localhost:3000",
+  baseURL: "https://one-6hcr.onrender.com", // Adjust the URL as needed
+  // baseURL: "http://localhost:3000",
   withCredentials: true, // for cookies
 });
 
