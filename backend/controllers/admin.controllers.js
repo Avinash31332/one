718c2f29cd0a41e7855d7d97dfbb755e1d5be1c1
Avinash <avinashviolin4@gmail.com>
2025-06-14 10:12:56 +0530

    fixed cookie and token issue

--- backend/controllers/admin.controllers.js
+++ backend/controllers/admin.controllers.js
@@ -113,7 +113,8 @@ export const loginAdmin = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        sameSite: "strict",
+        secure: true,
+        sameSite: "None", //for cross-site cookies
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
       .status(200)
